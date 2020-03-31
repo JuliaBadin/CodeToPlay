d1747521b89499e7f44e59e1912263da4cc1216a
Julia Badin <juliabadin03@gmail.com>
2020-03-31 16:10:33 -0300

    Login e Registro

---
+++ pages/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <script src="../js/index.js"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap" rel="stylesheet">
+        
+        <title>Projeto</title>
+        
+    </head>
+
+    <body>
+        
+        <?php include("login.php"); ?>
+        <button onClick=loginModal() class="button" > Entrar</button>
+
+        <?php include("registro.php"); ?>
+        <button onClick=registroModal() class="button" > Registrar-se</button>
+        
+        
+
+    </body>
+</html>
---
+++ pages/registro.php
@@ -0,0 +1,49 @@
+<div id="fundo-modal-registro" class="hide">
+    <div class="modal" id="modal-registro">
+
+        <div id="content">
+            <h2>Registrar-se</h2>
+            <form action="">
+
+                <div class="input-form">
+                    <label for="name">Seu nome completo</label>
+                    <input name="name" type="text" placeholder="Fulano da Silva">
+                </div>
+
+                <div class="input-form">
+                    <label for="email">Email</label>
+                    <input name="email" type="email" placeholder="exemplo@projeto.com">
+                </div>
+                
+                <div class="input-form">
+                    <label for="nickname">Nome de usuário</label>
+                    <input name="nickname" type="text" placeholder="Seu_Nome">
+                </div>
+
+                <div class="input-form">
+                    <label for="password">Senha</label>
+                    <input name="password" type="password" placeholder="********">
+                </div>
+
+                <div class="input-form">
+                    <label for="data_nascimento">Data de nascimento</label>
+                    <input name="data_nascimento" type="date" placeholder=" ">
+                </div>
+
+                <div class="input-form">
+                    <label for="nacionalidade">País de origem</label>
+                    
+                    <select name=Pais>
+                        <?php include("lista-paises.html"); ?>
+                    </select>
+                </div>
+                
+                <h6>Já tem uma conta?</h6>
+                <button class="button"><a href="./index.php" id="registrar-login">Entrar</a></button>
+                <input type="submit" value="Registrar-se" id="submit">
+
+            </form>
+        </div>
+
+    </div>
+</div>
