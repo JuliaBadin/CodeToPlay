e54b3ddb992cd6e8376bd41e2cd21fcf2610da60
camillyskk <camillyamoreira@gmail.com>
2021-02-15 14:25:33 -0300

    Add pagina de criaçao

--- pages/index.php
+++ pages/index.php
@@ -6,23 +6,23 @@
 
         <link rel="stylesheet" type="text/css" href="../css/style.css">
         <script src="../js/index.js"></script>
-        
+
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap" rel="stylesheet">
-        
+
         <title>Projeto</title>
-        
+
     </head>
 
     <body>
-        
+
         <?php include("login.php"); ?>
         <button onClick=loginModal() class="button" > Entrar</button>
 
         <?php include("registro.php"); ?>
         <button onClick=registroModal() class="button" > Registrar-se</button>
-        
-        
+
+
 
     </body>
 </html>
