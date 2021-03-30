b30840154af836fb823e1cd9d770789a09ce464c
JuliaBadin <juliabadin03@gmail.com>
2021-03-30 11:18:28 -0300

    arrumando database e pasta midia

--- database/db.sql
+++ database/db.sql
@@ -1,8 +1,14 @@
 CREATE DATABASE IF NOT EXISTS CodeToPlay;
 USE CodeToPlay;
 
+CREATE TABLE IF NOT EXISTS users(
+	idUser INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45),
+    password VARCHAR(45)
+);
+
 CREATE TABLE IF NOT EXISTS profile(
-	idProfile INT NOT NULL AUTO_INCREMENT,
+	idProfile INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
     surname VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
@@ -11,73 +17,64 @@ CREATE TABLE IF NOT EXISTS profile(
     birthday DATE,
     bio VARCHAR(200),
     users_idUser INT NOT NULL,
-    PRIMARY KEY(idProfile),
     FOREIGN KEY(users_idUser)
 		REFERENCES users(idUser)
 );
 
-CREATE TABLE IF NOT EXISTS users(
-	idUser INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45),
-    password VARCHAR(45),
-    PRIMARY KEY(idUser)
-);
-
 CREATE TABLE IF NOT EXISTS project(
-	idProject INT NOT NULL AUTO_INCREMENT,
-    code MEDIUMTEXT,
+	idProject INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    code LONGTEXT,
     name VARCHAR(45) NOT NULL,
     cover_page VARCHAR(45),
     users_idUser INT NOT NULL,
-    PRIMARY KEY (idProject),
     FOREIGN KEY (users_idUser)
 		REFERENCES users (idUser)
 );
 
 CREATE TABLE IF NOT EXISTS sounds(
-	idSound INT NOT NULL AUTO_INCREMENT,
+	idSound INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    link VARCHAR(100) NOT NULL,
-    PRIMARY KEY (idSound)
+    link VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS characters(
-	idCharacter INT NOT NULL AUTO_INCREMENT,
+	idCharacter INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    link VARCHAR(100) NOT NULL,
-    PRIMARY KEY (idCharacter)
+    link VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS scenarios(
-	idScenario INT NOT NULL AUTO_INCREMENT,
+	idScenario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    link VARCHAR(100) NOT NULL,
-    PRIMARY KEY (idScenario)
+    link VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS project_has_scenarios(
 	project_idProject INT NOT NULL,
     scenarios_idScenario INT NOT NULL,
     FOREIGN KEY (project_idProject)
-		REFERENCES project (id_Project),
+		REFERENCES project (idProject),
 	FOREIGN KEY (scenarios_idScenario)
-		REFERENCES scenarios (idScenario)
+		REFERENCES scenarios (idScenario),
+	PRIMARY KEY (project_idProject, scenarios_idScenario)
 );
 
 CREATE TABLE IF NOT EXISTS project_has_sounds(
 	project_idProject INT NOT NULL,
     sounds_idSound INT NOT NULL,
     FOREIGN KEY (project_idProject)
-		REFERENCES project (id_Project),
+		REFERENCES project (idProject),
 	FOREIGN KEY (sounds_idSound)
-		REFERENCES sounds (idSound)
+		REFERENCES sounds (idSound),
+	PRIMARY KEY (project_idProject, sounds_idSound)
 );
 
 CREATE TABLE IF NOT EXISTS project_has_characters(
 	project_idProject INT NOT NULL,
     characters_idCharacter INT NOT NULL,
     FOREIGN KEY (project_idProject)
-		REFERENCES project (id_Project),
+		REFERENCES project (idProject),
 	FOREIGN KEY (characters_idCharacter)
-		REFERENCES characters (idCharacter)
+		REFERENCES characters (idCharacter),
+	PRIMARY KEY (project_idProject, characters_idCharacter)
 );
