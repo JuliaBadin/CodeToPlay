70aba41632ce40225ce8319f41645b9f7beac744
Julia Badin <juliabadin03@gmail.com>
2021-10-18 18:13:31 -0300

    alterações no banco

---
+++ database/db-dados.sql
@@ -0,0 +1,150 @@
+/*          ---------------          PERSONAGENS          ---------------          */
+
+INSERT INTO `characters`(`name`, `link`) VALUES ('alien','../../midia/images/characters/alien_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('bola de vôlei','../../midia/images/characters/ball_beach.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('bola de futebol','../../midia/images/characters/ball_soccer.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('pássaro azul','../../midia/images/characters/bird_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('mulher de negócios 1','../../midia/images/characters/business_woman_idea.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('mulher de negócios 2','../../midia/images/characters/business_woman_sitting.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('gato','../../midia/images/characters/cat.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo preto','../../midia/images/characters/circle_black.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo azul','../../midia/images/characters/circle_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo verde','../../midia/images/characters/circle_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo rosa','../../midia/images/characters/circle_pink.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo vermelho','../../midia/images/characters/circle_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo branco','../../midia/images/characters/circle_white.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('círculo amarelo','../../midia/images/characters/circle_yellow.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('cachorro','../../midia/images/characters/dog.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('planeta terra','../../midia/images/characters/earth.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('explosão','../../midia/images/characters/explosion.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('planeta júpiter','../../midia/images/characters/jupiter.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('duende','../../midia/images/characters/leprechaun.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('planeta marte','../../midia/images/characters/mars.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('monstro azul','../../midia/images/characters/monster_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('monstro vermelho 1','../../midia/images/characters/monster_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('monstro verde','../../midia/images/characters/monster_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('monstro laranja','../../midia/images/characters/monster_orange.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('homem velho','../../midia/images/characters/old_man.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('mulher velha 1','../../midia/images/characters/old_woman_1.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('mulher velha 2','../../midia/images/characters/old_woman_2.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo preto','../../midia/images/characters/rectangle_black.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo azul','../../midia/images/characters/rectangle_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo verde','../../midia/images/characters/rectangle_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo vermelho','../../midia/images/characters/rectangle_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo branco','../../midia/images/characters/rectangle_white.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('retângulo amarelo','../../midia/images/characters/rectangle_yellow.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('robô','../../midia/images/characters/robot.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('nave espacial','../../midia/images/characters/spaceship.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('planeta saturno','../../midia/images/characters/saturn.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado preto','../../midia/images/characters/square_black.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado azul','../../midia/images/characters/square_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado verde','../../midia/images/characters/square_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado rosa','../../midia/images/characters/square_pink.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado vermelho','../../midia/images/characters/square_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado branco','../../midia/images/characters/square_white.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('quadrado amarelo','../../midia/images/characters/square_yellow.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela preta','../../midia/images/characters/star_black.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela azul','../../midia/images/characters/star_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela verde','../../midia/images/characters/star_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela rosa','../../midia/images/characters/star_pink.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela vemelho','../../midia/images/characters/star_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela branca','../../midia/images/characters/star_white.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('estrela amarela','../../midia/images/characters/star_yellow.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('garoto estudante','../../midia/images/characters/student_boy.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo preto','../../midia/images/characters/triangle_black.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo azul','../../midia/images/characters/triangle_blue.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo verde','../../midia/images/characters/triangle_green.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo rosa','../../midia/images/characters/triangle_pink.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo vermelho','../../midia/images/characters/triangle_red.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo branco','../../midia/images/characters/triangle_white.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('triângulo amarelo','../../midia/images/characters/triangle_yellow.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('nave alienígena','../../midia/images/characters/ufo.png');
+INSERT INTO `characters`(`name`, `link`) VALUES ('bruxa','../../midia/images/characters/witch.png');
+
+
+
+/*          ---------------          CENÁRIOS          ---------------          */
+
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('praia','../../midia/images/scenarios/beach.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('cafeteria retrô','../../midia/images/scenarios/cafeteria_retro.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('cidade de noite','../../midia/images/scenarios/city_night.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('cidade de noite chovendo','../../midia/images/scenarios/dark_city_raining.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('deserto 1','../../midia/images/scenarios/desert_1.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('deserto 2','../../midia/images/scenarios/desert_2.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('deserto 3','../../midia/images/scenarios/desert_3.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('entrada do câmpus','../../midia/images/scenarios/entrance_university.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('fazenda','../../midia/images/scenarios/farm.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('floresta 1','../../midia/images/scenarios/forest.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('floresta 2','../../midia/images/scenarios/forest_2.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('floresta de noite','../../midia/images/scenarios/forest_dark.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('galáxia','../../midia/images/scenarios/galaxy.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('hospital','../../midia/images/scenarios/hospital.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('floresta 3','../../midia/images/scenarios/jungle.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('cozinha','../../midia/images/scenarios/kitchen.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('laboratório','../../midia/images/scenarios/lab.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('lago de noite','../../midia/images/scenarios/lake_night.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('mansão de noite','../../midia/images/scenarios/mansion_night.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('bomba nuclear','../../midia/images/scenarios/nuclear_bomb.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('navio pirata 1','../../midia/images/scenarios/pirate_ship.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('navio pirata 2','../../midia/images/scenarios/pirate_ship_2.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('corredar da escola','../../midia/images/scenarios/school_highway.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('mar','../../midia/images/scenarios/sea.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('fundo do mar','../../midia/images/scenarios/seabed.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('céu','../../midia/images/scenarios/sky.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('céu noturno','../../midia/images/scenarios/sky_night.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('céu com nuvens rosas','../../midia/images/scenarios/sky_pink_clouds.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('céu ensolarado','../../midia/images/scenarios/sky_sun.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('preto sólido','../../midia/images/scenarios/solid_black.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('azul sólido','../../midia/images/scenarios/solid_blue.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('verde sólido','../../midia/images/scenarios/solid_green.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('rosa sólido','../../midia/images/scenarios/solid_pink.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('vermelho sólido','../../midia/images/scenarios/solid_red.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('branco sólido','../../midia/images/scenarios/solid_white.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('amarelo sólido','../../midia/images/scenarios/solid_yellow.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('planeta alienígena','../../midia/images/scenarios/space_alien.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('praça 1','../../midia/images/scenarios/square_1.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('praça 2','../../midia/images/scenarios/square_2.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('supermercado','../../midia/images/scenarios/supermarket.jpg');
+INSERT INTO `scenarios`(`name`, `link`) VALUES ('vista da cidade de noite','../../midia/images/scenarios/view_of_city_night.jpg');
+
+
+
+/*          ---------------          SONS          ---------------          */
+
+INSERT INTO `sounds`(`name`, `link`) VALUES ('robô','../../midia/sounds/activated_robot.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('planeta alienígena','../../midia/sounds/alien_planet_atmosphere.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('beep 1','../../midia/sounds/beep_1.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('beep 2','../../midia/sounds/beep_2.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('beep 3','../../midia/sounds/beep_3.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('piado único','../../midia/sounds/bird_chirp_unique.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('oceano e pássaros','../../midia/sounds/birds_and_ocean_waves.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('vidro quebrando','../../midia/sounds/broken_glass.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('camera','../../midia/sounds/camera.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('risada','../../midia/sounds/cartoon_laugh.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('cidade','../../midia/sounds/city_atmosphere.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('moeda','../../midia/sounds/coin.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('atmosfera sinistra','../../midia/sounds/dark_atmosphere.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('risada demoníaca','../../midia/sounds/devil_laugh.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('fazenda','../../midia/sounds/farm.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('floresta','../../midia/sounds/forest.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('fantasma','../../midia/sounds/gosth.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('lama','../../midia/sounds/impact_on_dirt.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('ondas do lago','../../midia/sounds/lakes_waves.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('miado 1','../../midia/sounds/meow_1.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('miado 2','../../midia/sounds/meow_2.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('música com atmosfera sinistra','../../midia/sounds/music_dark_atmosphere.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('coruja','../../midia/sounds/owl_hoot.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('pirata','../../midia/sounds/pirate.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('ondas do mar','../../midia/sounds/sea_waves.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('tiro 1','../../midia/sounds/shoot_1.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('tiro 2','../../midia/sounds/shoot_2.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('zumbi 1','../../midia/sounds/zombie_1.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('zumbi 2','../../midia/sounds/zombie_2.mp3');
+INSERT INTO `sounds`(`name`, `link`) VALUES ('zumbi 3','../../midia/sounds/zombie_3.mp3');
+
+
+
+
+/*          ---------------          OTHERS          ---------------          */
+
+INSERT INTO `users`(`name`, `link`) VALUES ('user_padrao','../../midia/images/users/user_padrao.png');
